f411289d56c2d87e95c374aef722b60a2de511d9
Aurimas Liutikas <aurimas@google.com>
2024-09-04 14:09:36 -0700

    Add hardcoding to the locations of prebuilts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeCompile
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
@@ -18,4 +20,17 @@ kotlin {
         desktopMain.dependencies {
         }
     }
+}
+
+extensions.extraProperties["kotlin.native.distribution.baseDownloadUrl"] = "file:/usr/local/google/home/aurimas/Code/androidx-main/prebuilts/androidx/konan/nativeCompilerPrebuilts"
+
+tasks.withType(KotlinNativeCompile::class.java).configureEach {
+    compilerOptions.freeCompilerArgs.add(
+        "-Xoverride-konan-properties=dependenciesUrl=file:/usr/local/google/home/aurimas/Code/androidx-main/prebuilts/androidx/konan"
+    )
+}
+
+tasks.withType(CInteropProcess::class.java).configureEach {
+    settings.extraOpts +=
+        listOf("-Xoverride-konan-properties", "dependenciesUrl=file:/usr/local/google/home/aurimas/Code/androidx-main/prebuilts/androidx/konan")
 }
